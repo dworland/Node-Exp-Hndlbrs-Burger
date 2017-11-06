63b5f189ceb2e50949bf7d3a28defcc62e88c7c4
Devon Worland <devonworland@Devons-MacBook-Pro.local>
2017-11-06 13:46:39 -0600

    database created, connection and orm complete

--- config/orm.js
+++ config/orm.js
@@ -1,4 +1,67 @@
 var connection = require("./connection.js");
-var ORM;
+var orm;
 
-module.exports = ORM;
+function printQuestionMarks(num) {
+	var arr = [];
+
+	for (var i = 0; i < num; i++) {
+		arr.push("?");
+	}
+
+	return arr.toString();
+}
+
+function objToSql(obj) {
+	var arr = [];
+
+	for (var key in obj) {
+		arr.push(key + " = " + obj[key]);
+	}
+
+	return arr.toString();
+}
+
+var orm = {
+	selectAll: function(tableInput, callback) {
+		var queryString = "SELECT * FROM " + tableInput;
+		connection.query(queryString, function(err, result) {
+			if (err) throw err;
+			callback(result);
+		});
+	},
+
+	insertOne: function(table, cols, vals, callback) {
+		var queryString = "INSERT INTO " + table;
+
+		queryString += " (";
+	    queryString += cols.toString();
+	    queryString += ") ";
+	    queryString += "VALUES (";
+	    queryString += printQuestionMarks(vals.length);
+	    queryString += ") ";
+
+	    connection.query(queryString, vals, function(err, result) {
+	    	if (err) throw err;
+	    	callback(result);
+	    });
+	},
+
+	updateOne: function(table, objColVals, condition, callback) {
+		var queryString = "UPDATE " + table;
+
+		queryString += " SET ";
+	    queryString += objToSql(objColVals);
+	    queryString += " WHERE ";
+	    queryString += condition;
+
+	    connection.query(queryString, function(err, result) {
+	    	if (err) throw err;
+	    	callback(result);
+	    });
+
+	}
+
+};
+
+module.exports = orm;
+
